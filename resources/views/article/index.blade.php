897090dadc1c1380f0bf234814167560454a4391
ta <tatevikg1@gmail.com>
2020-12-24 22:31:02 +0400

    package.json postinstall

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -29,13 +29,13 @@
                                     <td>
                                         <a href='articles/{{ $article->id }}/edit' class="btn btn-outline-secondary">Edit</a>
                                     </td>
-                                @elseif ($article->sent == 'true' )
+                                @elseif ($article->sent == 'true')
                                     <td>
                                         <a href='articles/{{ $article->id  }}' class="btn btn-outline-secondary">Sent</a>
                                     </td>
                                 @else
                                     <td>
-                                        <send-button url="/articles/{{ $article->id }}/edit" article-id="{{ $article->id }}" sent="{{ true }}"></send-button>
+                                        <send-button url="/articles/{{ $article->id }}/edit" article-id="{{ $article->id }}" sent="{{ $article->sent }}"></send-button>
                                     </td>
                                 @endif
                             </tr>
