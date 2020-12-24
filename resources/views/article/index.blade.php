8e28e3c06c3f04ac69b1825b55fb3ff85beb7f19
ta <tatevikg1@gmail.com>
2020-12-24 21:32:48 +0400

    test

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Articles') }}</div>
+                <div class="card-header">{{ __('Articles') }} bla bla</div>
 
                 <div class="card-body">
                     <table class="table">
