dc89c3c1d92becc1a0bd8d3fc6f52d9ed806422e
ta <tatevikg1@gmail.com>
2021-01-13 23:10:52 +0400

    v-on:click

--- resources/js/components/PublishButton.vue
+++ resources/js/components/PublishButton.vue
@@ -1,12 +1,9 @@
 <template>
     <div>
-        <form v-on:submit.prevent="publish">
-            <input type="submit" @click.prevent="publish"  v-show="status" class= "btn btn-outline-danger" text="Publish">
-        </form>
+        <button v-on:click="publish"  v-show="status" class= "btn btn-outline-danger">Publish</button>
         
         <a v-bind:href="url" class='btn btn-outline-primary' v-show="status">Edit</a>
 
-        <!-- <p class='btn btn-outline-success' v-show="!status">Published</p> -->
         <a v-bind:href="articleId" class='btn btn-outline-success' v-show="!status">Published</a>
     </div>
 </template>
@@ -32,23 +29,23 @@
         methods: {
             publish() {
                 alert('published');
-                axios.post('/publish/'+ this.articleId)
-                    .then(response => {
+                // axios.post('/publish/'+ this.articleId)
+                //     .then(response => {
 
-                        // if (!this.status) {
-                        //     this.url = '';
-                        //     return
-                        // }
+                //         // if (!this.status) {
+                //         //     this.url = '';
+                //         //     return
+                //         // }
 
-                        this.status = ! this.status;
+                //         this.status = ! this.status;
 
-                        console.log(response.data);
-                    })
-                    .catch(errors => {
-                        if (errors.response.status == 401) {
-                            window.location = '/login';
-                        }
-                    });
+                //         console.log(response.data);
+                //     })
+                //     .catch(errors => {
+                //         if (errors.response.status == 401) {
+                //             window.location = '/login';
+                //         }
+                //     });
             }
         }
     }
